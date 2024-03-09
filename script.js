5e0286c8acec7627cf8046749cd584628a807e0e
Rahul-Pala <rahul_pala@hotmail.com>
2024-03-09 14:40:39 +1300

    Working as expected. added tests

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function diceroll(max)
         {
-            return Math.floor(Math.random() * max + 1);
+           
+            let result= Math.floor(Math.random() * max + 1);
+            if(result > max) {throw new Error("result is more than max")}
+            if(result < 1) {throw new Error("result is less than one")}
+            if(max===6)document.getElementById("result6").innerText=result
+            if(max===10)document.getElementById("result10").innerText=result
         }
 
-console.log (diceroll(10))
-console.log (diceroll(10))
-console.log (diceroll(10))
-console.log (diceroll(10))
-console.log (diceroll(10))
-console.log (diceroll(10))
-console.log (diceroll(10))
-console.log (diceroll(10))
-console.log (diceroll(10))
-console.log (diceroll(10))
-console.log (diceroll(10))
+    
+
+
+
+
+
+
+     
 
 
