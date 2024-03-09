4baae629980bf661ab9987cda70b42e02cdf237c
Rahul-Pala <rahul_pala@hotmail.com>
2024-03-09 14:24:50 +1300

    merge branches

---
+++ script.js
@@ -0,0 +1,18 @@
+function diceroll(max)
+        {
+            return Math.floor(Math.random() * max + 1);
+        }
+
+console.log (diceroll(10))
+console.log (diceroll(10))
+console.log (diceroll(10))
+console.log (diceroll(10))
+console.log (diceroll(10))
+console.log (diceroll(10))
+console.log (diceroll(10))
+console.log (diceroll(10))
+console.log (diceroll(10))
+console.log (diceroll(10))
+console.log (diceroll(10))
+
+
